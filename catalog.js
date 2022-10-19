0db10e5830862c9d29a44fb31a02026c9669a773
ALena Demina <w.demina.alena@gmail.com>
2022-10-19 16:14:07 +0400

    feat: add products catalog

---
+++ catalog.js
@@ -0,0 +1,68 @@
+const headphones = [
+    {
+        id: "product1",
+        img: "./assets/S852.png",
+        title: "Apple BYZ S8521",
+        price: 2927,
+        rate: 4.7,
+    },
+    {
+        id: "product2",
+        img: "./assets/earpods1.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate: 4.5,
+    },
+    {
+        id: "product3",
+        img: "./assets/earpods2.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate: 4.5,
+    },
+    {
+        id: "product4",
+        img: "./assets/S852.png",
+        title: "Apple BYZ S8521",
+        price: 2927,
+        rate: 4.7,
+    },
+    {
+        id: "product5",
+        img: "./assets/earpods1.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate: 4.5,
+    },
+    {
+        id: "product6",
+        img: "./assets/earpods2.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate: 4.5,
+    },
+]
+
+const wireless = [
+    {
+        id: "product7",
+        img: "./assets/airpods.png",
+        title: "Apple AirPods",
+        price: 9527,
+        rate: 4.7,
+    },
+    {
+        id: "product8",
+        img: "./assets/gerlax.png",
+        title: "GERLAX GH-04",
+        price: 6527,
+        rate: 4.7,
+    },
+    {
+        id: "product9",
+        img: "./assets/borofone.png",
+        title: "BOROFONE B04",
+        price: 7527,
+        rate: 4.7,
+    },
+]
